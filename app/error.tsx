9fe4726d7593638286f2c3b3cdc2af4abd9de136
Manjyot Kalkat <57817155+ManjyotK@users.noreply.github.com>
2024-07-31 18:33:26 -0500

    format error page

--- app/error.tsx
+++ app/error.tsx
@@ -18,15 +18,15 @@ export default function Error({
   }, [error])
  
   return (
-    <div>
-      <h2>{errorMessage}</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }>
-        Try again
-      </button>
+    <div className='flex flex-col items-center justify-center w-full'>
+      <h2 className='text-2xl font-semibold'>{errorMessage}</h2>
+      <div>
+        <button
+          className='mt-4 px-4 py-2 border border-gray-300 rounded-md shadow-sm hover:bg-gray-100'
+          onClick={() => reset()}>
+          Try Again
+        </button>
+      </div>
     </div>
   )
 }
